b22792c80301c7bec67708d5542fd99e304d67f5
Nick <nickcengel@gmail.com>
2018-06-29 20:14:50 -0600

    Final Serial, handles buffer overflow errors

---
+++ Serial_FileModel/Serial_FileModel/firmware/Serial_FileModel.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Serial_FileModel.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Serial_FileModel.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=serialfilemodel.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/serialfilemodel.x.tar
